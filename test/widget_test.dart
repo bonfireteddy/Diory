7ba8fba1f886ead6d128fb964ff3b32e1a4de90f
202002575 <eogkrry2020@gmail.com>
2022-12-26 18:54:37 +0900

    Main Homepage + Drawer menu

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:diory/main.dart';
+import 'package:diory_project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
