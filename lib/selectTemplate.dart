14f10ba517204196eee7919e885519a57d1715b2
als8921 <als8921@naver.com>
2022-12-27 14:22:57 +0900

    템플릿 선택화면 UI 완성

--- lib/selectTemplate.dart
+++ lib/selectTemplate.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+const basicTemplateList = [
+  "Basic 1",
+  "Basic 2",
+  "Basic 3",
+  "Basic 4",
+  "Basic 5"
+];
+const customTemplateList = [
+  "Custom 1",
+  "Custom 2",
+  "Custom 3",
+  "Custom 4",
+  "Custom 5"
+];
+const downloadTemplateList = [
+  "Download 1",
+  "Download 2",
+  "Download 3",
+  "Download 4",
+  "Download 5",
+];
+
 class SelectTemplatePage extends StatefulWidget {
   const SelectTemplatePage({super.key});
   @override
@@ -31,25 +53,15 @@ class _SelectTemplatePageState extends State<SelectTemplatePage> {
         Container(
           padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
           height: 190.0,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              _pageComponent("111"),
-              const SizedBox(width: 15),
-              _pageComponent("222"),
-              const SizedBox(width: 15),
-              _pageComponent("333"),
-              const SizedBox(width: 15),
-              _pageComponent("111"),
-              const SizedBox(width: 15),
-              _pageComponent("222"),
-              const SizedBox(width: 15),
-              _pageComponent("333"),
-            ],
-          ),
+          child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: basicTemplateList.length,
+              itemBuilder: ((context, idx) =>
+                  _pageComponent(basicTemplateList[idx])),
+              separatorBuilder: (context, idx) => const SizedBox(width: 15)),
         ),
         Container(
-          padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
           child: const Text(
             "커스텀 템플릿",
             style: TextStyle(fontSize: 30),
@@ -58,25 +70,15 @@ class _SelectTemplatePageState extends State<SelectTemplatePage> {
         Container(
           padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
           height: 190.0,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              _pageComponent("111"),
-              const SizedBox(width: 15),
-              _pageComponent("222"),
-              const SizedBox(width: 15),
-              _pageComponent("333"),
-              const SizedBox(width: 15),
-              _pageComponent("111"),
-              const SizedBox(width: 15),
-              _pageComponent("222"),
-              const SizedBox(width: 15),
-              _pageComponent("333"),
-            ],
-          ),
+          child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: customTemplateList.length,
+              itemBuilder: ((context, idx) =>
+                  _pageComponent(customTemplateList[idx])),
+              separatorBuilder: (context, idx) => const SizedBox(width: 15)),
         ),
         Container(
-          padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
           child: const Text(
             "다운로드 템플릿",
             style: TextStyle(fontSize: 30),
@@ -85,23 +87,13 @@ class _SelectTemplatePageState extends State<SelectTemplatePage> {
         Container(
           padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
           height: 190.0,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              _pageComponent("111"),
-              const SizedBox(width: 15),
-              _pageComponent("222"),
-              const SizedBox(width: 15),
-              _pageComponent("333"),
-              const SizedBox(width: 15),
-              _pageComponent("111"),
-              const SizedBox(width: 15),
-              _pageComponent("222"),
-              const SizedBox(width: 15),
-              _pageComponent("333"),
-            ],
-          ),
-        )
+          child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: downloadTemplateList.length,
+              itemBuilder: ((context, idx) =>
+                  _pageComponent(downloadTemplateList[idx])),
+              separatorBuilder: (context, idx) => const SizedBox(width: 15)),
+        ),
       ]),
     );
   }
