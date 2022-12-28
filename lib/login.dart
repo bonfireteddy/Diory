5cdf1ecb0eb8128682b85fd6b5888b92ed3aff3e
sseooh <sseooh@gmail.com>
2022-12-28 13:05:05 +0900

    login

---
+++ lib/login.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+import 'homepage.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providerConfigs: const [
+              EmailProviderConfiguration(),
+              GoogleProviderConfiguration(
+                  clientId:
+                  '963656261848-v7r3vq1v6haupv0l1mdrmsf56ktnua60.apps.googleusercontent.com'),
+            ],
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Welcome to Diory, please sign in!')
+                    : const Text('Welcome to Diory, please sign up!'),
+              );
+            },
+          );
+        }
+
+        return const MyHomePage();
+      },
+    );
+  }
+}
