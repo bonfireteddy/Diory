1dded0715d2a7a9c889ad493b3f4c8a249535ed3
als8921 <als8921@naver.com>
2022-12-28 11:08:37 +0900

    EditPage

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,8 @@ class MyApp extends StatelessWidget {
             elevation: 0,
           ),
           textTheme: const TextTheme()),
-      home:
-          const MyHomePage(), //여기에서 처음에는 로그인 및 회원가입 화면으로, 이후 로그인상태에서는 홈화면으로 이동
+      home: const edit.MyHomePage(
+          title: '테스트'), //여기에서 처음에는 로그인 및 회원가입 화면으로, 이후 로그인상태에서는 홈화면으로 이동
     );
   }
 }
